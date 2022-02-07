ebfd18a0109db6abdec508ac3b2d2cf157155a3e
eric-gcv <eric.gcv@gmail.com>
2022-02-07 11:56:54 +0100

    first site build

---
+++ setup.R
@@ -0,0 +1,18 @@
+# SET UP
+
+install.packages("blogdown")
+library(blogdown)
+
+blogdown::hugo_version()
+blogdown::install_hugo()
+
+blogdown::new_site(theme = "gethugothemes/infinity-hugo")
+
+# To start a local preview: use blogdown::serve_site(), or the RStudio add-in "Serve Site"
+# To stop a local preview: use blogdown::stop_server(), or restart your R session
+# Then visit http://localhost:7715/ in your browser to view the example site.
+blogdown::serve_site()
+blogdown::stop_server()
+
+# run in terminal to force build
+curl -X POST -d '{}' https://api.netlify.com/build_hooks/60c0c716ac8ece1126e7d908
